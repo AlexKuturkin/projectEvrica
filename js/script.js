4e8fa1b67b010bc43c51e176bc28b4d9da0a9ad3
Alex <a.kuturkin@mail.ru>
2019-10-02 13:22:55 +0300

    New

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ navImage.addEventListener('mouseout', function () {
     navImage.src = './images/search icon.svg';
 });
 
+
 const bannerButtonPhone = document.querySelector('.banner__button_phone');
 const bannerLogoPhone = document.querySelector('.banner__logo_phone');
 
@@ -18,3 +19,22 @@ bannerButtonPhone.addEventListener('mouseout', function () {
     bannerLogoPhone.src = './images/phone icon.svg';
 });
 
+
+const mapMark = document.querySelector('.map__mark');
+mapMark.addEventListener('mouseover', function () {
+    mapMark.src = './images/pin copy 6 hover.svg';
+});
+
+mapMark.addEventListener('mouseout', function () {
+    mapMark.src = './images/pin copy 6.svg';
+});
+
+const mapDiv = document.querySelector('.map__div');
+mapMark.addEventListener('click', function () {
+    mapDiv.classList.add('map__div_active');
+});
+
+const mapLogoClose = document.querySelector('.map__logo_close');
+mapLogoClose.addEventListener('click', function () {
+    mapDiv.classList.remove('map__div_active');
+});
