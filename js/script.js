d2e1ee52cfd5d5ad538709382b60a3b60dda2054
Alex <a.kuturkin@mail.ru>
2019-10-02 13:29:36 +0300

    n

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const navImage = document.querySelector('.nav__image');
 navImage.addEventListener('mouseover', function () {
     navImage.src = './images/hover.svg';
 });
-
 navImage.addEventListener('mouseout', function () {
     navImage.src = './images/search icon.svg';
 });
@@ -10,11 +9,9 @@ navImage.addEventListener('mouseout', function () {
 
 const bannerButtonPhone = document.querySelector('.banner__button_phone');
 const bannerLogoPhone = document.querySelector('.banner__logo_phone');
-
 bannerButtonPhone.addEventListener('mouseover', function () {
     bannerLogoPhone.src = './images/phone icon copy.svg';
 });
-
 bannerButtonPhone.addEventListener('mouseout', function () {
     bannerLogoPhone.src = './images/phone icon.svg';
 });
@@ -24,17 +21,23 @@ const mapMark = document.querySelector('.map__mark');
 mapMark.addEventListener('mouseover', function () {
     mapMark.src = './images/pin copy 6 hover.svg';
 });
-
 mapMark.addEventListener('mouseout', function () {
     mapMark.src = './images/pin copy 6.svg';
 });
-
 const mapDiv = document.querySelector('.map__div');
 mapMark.addEventListener('click', function () {
     mapDiv.classList.add('map__div_active');
 });
-
 const mapLogoClose = document.querySelector('.map__logo_close');
 mapLogoClose.addEventListener('click', function () {
     mapDiv.classList.remove('map__div_active');
+});
+
+
+const mapHide = document.querySelector('.map__hide');
+mapHide.addEventListener('mouseover', function () {
+    mapHide.src = './images/свернуть hover.png';
+});
+mapHide.addEventListener('mouseout', function () {
+    mapHide.src = './images/свернуть.png';
 });
